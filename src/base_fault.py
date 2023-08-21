283d58c8d72dd3f6eca0b124484d0ecece11f587
Akram Batikh <asbatikh@ncsu.edu>
2023-08-21 14:47:44 -0400

    Fault tree and gates desc files are written using write_mainshock_fault_tree method

--- src/base_fault.py
+++ src/base_fault.py
@@ -4,6 +4,42 @@ from abc import ABC, abstractmethod
 
 class BaseFaultTree:
 
+    def __init__(self, event_description, phase_type, probability_parameters, event_name, project_name, analysis_type,
+                 Lambda, tau, prob, mission, UdC, FdT, UdValue, UdValue2, init, PF, Freq, Tau, beta_u, beta_r, a_m, building, floor, elevation, area, count,
+                 seismic_fire,seismic_flooding,peak_ground_accel,start_time,end_time):
+        self.event_description = event_description
+        self.phase_type = phase_type
+        self.probability_parameters = probability_parameters
+        self.event_name = event_name
+        self.project_name = project_name
+        self.analysis_type = analysis_type  #
+        self.Lambda = Lambda  # Basic event failure rate per hr
+        self.tau = tau  # Time to repair in hours
+        self.prob = prob  # Probability value
+        self.mission = mission  # Mission time
+        self.UdC = UdC  # Uncertainty correlation class
+        self.FdT = FdT  # Failure Calculation type
+        self.UdValue = UdValue  # Uncertainty distribution value
+        self.UdValue2 = UdValue2
+        self.init = init  # Initiating event flag (Y/N)
+        self.PF = PF  # Process Flag
+        self.Freq = Freq  # Frequency Unit
+        self.Tau = Tau
+
+        self.beta_u = beta_u
+        self.beta_r = beta_r
+        self.a_m = a_m
+        self.building = building
+        self.floor = floor
+        self.elevation = elevation
+        self.area = area
+        self.seismic_fire = seismic_fire
+        self.seismic_flooding = seismic_flooding
+        self.peak_ground_accel = peak_ground_accel
+        self.count = count
+        self.start_time = start_time
+        self.end_time = end_time
+
     def write_file(self, file_path, content):
         if not os.path.exists(file_path):
             with open(file_path, 'w') as file:
--- src/fault_tree.py
+++ src/fault_tree.py
@@ -3,6 +3,20 @@ from base_fault import BaseFaultTree
 from seismic_event import SeismicEvent
 
 class FaultTree(BaseFaultTree):
+
+    def __init__(self, event_description="", phase_type="", probability_parameters=None,
+                 event_name="", project_name="", analysis_type="", Lambda=0, tau=0, prob=0,
+                 mission=0, UdC="", FdT=0, UdValue=0, UdValue2=0, init=0, PF=0, Freq=0, Tau = 0,
+                 beta_u=0, beta_r=0, a_m=0, building="", floor=0, elevation=0, area="",
+                 count=0, seismic_fire="", seismic_flooding="", peak_ground_accel=0,start_time= 0, end_time =24):
+        super().__init__(event_description, phase_type, probability_parameters,
+                         event_name, project_name, analysis_type, Lambda, tau, prob,
+                         mission, UdC, FdT, UdValue, UdValue2, init, PF, Freq,Tau ,beta_u,
+                         beta_r, a_m, building, floor, elevation, area, count,
+                         seismic_fire, seismic_flooding, peak_ground_accel,start_time,end_time)
+        self.aftershocks_params = None
+        self.mainshock_params = None
+        self.frequency_event_processed = False  # Flag to track if frequency event has been processed
     gate_name_categories = {}  # Define the class-level attribute
     fault_name_categories = {}
     def write_mainshock_fault_tree(self, output_directory,Json_input,ms_event_names,he_ms_names):
@@ -14,15 +28,14 @@ class FaultTree(BaseFaultTree):
         project_name = seismic_event_info.analysis_params["project"]
 
         # Fault Tree logic file (FTL)
-        file_path = os.path.join(output_directory, "fault_tree.ftl")  # FTL file path
-
-
-
-
-
+        file_path_ftl = os.path.join(output_directory, "fault_tree.FTL")  # FTL file path
+        file_path_ftd = os.path.join(output_directory, "fault_tree.FTD")  # FTD file path
+        file_path_gtd = os.path.join(output_directory, "fault_tree.GTD")  # FTD file path
         reshaped_event_names = np.reshape(ms_event_names, (-1, num_mainshock_intervals))
 
-        with open(file_path, 'w') as file:
+        with open(file_path_ftl, 'a') as file_ftl, open(file_path_ftd, 'a') as file_ftd, open(file_path_gtd,'a') as file_gtd:
+            file_ftd.write(f"{project_name}      =  \n*  Name                 , Description,        SubTree, Alternate, Project\n")
+            file_gtd.write(f"{project_name}      =  \n*  Name                 , Description, Project\n")
             for row in reshaped_event_names:
 
                 name_split = row[0].split("-")[:-1]
@@ -32,28 +45,38 @@ class FaultTree(BaseFaultTree):
                 # Join the modified elements back together with "-"
                 combined_name = "-".join(modified_name_split)
 
-                ms_ft_name = f"{project_name} ,   {combined_name}-FT = "
-                file.write(ms_ft_name + '\n')
+                fault_tree_name = f"{combined_name}-FT"
+                self.collect_fault_tree_name("MS",fault_tree_name)
+                file_ftd.write(f"{fault_tree_name}           , MAINSHOCK FAULT TREE OF                 , S,  , {project_name}\n")
+                ms_ft_name = f"{project_name} ,   {fault_tree_name} = "
+                file_ftl.write(ms_ft_name + '\n')
 
 
-                ms_ft_name = f"{combined_name}-FT              OR "
-                file.write(ms_ft_name)
+                ms_ft_name = f"{fault_tree_name}             OR "
+                file_ftl.write(ms_ft_name)
                 for event_name in row:
+
+                    bin =self.extract_element_after_ms(event_name)
+
+
                     # Add "GT" prefix to the event name and write it to the file
-                    gt_name = f"{event_name}-GT  "
-                    self.collect_gate_name("MS",gt_name)
-                    file.write(gt_name)
-                file.write('\n')
+                    gate_name = f"{event_name}-GT  "
+                    file_gtd.write(f"{gate_name}  , MAINSHOCK BIN {bin} , {project_name}\n")
+                    self.collect_gate_name("MS",gate_name)
+                    file_ftl.write(gate_name)
+                file_ftl.write('\n')
 
                 for index ,event_name in enumerate(row):
                     gt_comp = f"{event_name}-GT                  AND  "
-                    file.write(gt_comp)
-                    file.write(f"{he_ms_names[index]} {event_name}\n")
-                file.write("^EOS\n")
+                    file_ftl.write(gt_comp)
+                    file_ftl.write(f"{he_ms_names[index]} {event_name}\n")
+                file_ftl.write("^EOS\n")
 
 
 
         # Write fault tree description file
+
+
 
 
     @classmethod
@@ -68,9 +91,17 @@ class FaultTree(BaseFaultTree):
             cls.fault_name_categories[category] = []
         cls.fault_name_categories[category].append(fault_tree_name)
 
-
-
-
+    @classmethod
+    def extract_element_after_ms(cls,text):
+        elements = text.split("-")
+        try:
+            ms_index = elements.index("MS")
+            if ms_index < len(elements) - 1:
+                return elements[ms_index + 1]
+            else:
+                return None  # No element after "MS"
+        except ValueError:
+            return None  # "MS" not found
 
 
 
@@ -104,3 +135,30 @@ class FaultTree(BaseFaultTree):
 
 
 
+    def from_csv(cls, path,seismic_event_csv):  # This method reads the seismic event csv file
+        complete_path = os.path.join(path, seismic_event_csv)
+
+        events = []
+        seismic_event_data = pd.read_csv(complete_path)
+        for _, row in seismic_event_data.iterrows():
+            event = cls()
+            event.event_description = row['Event description']
+            event.event_name = row['Event name']
+            event.beta_u = row['BetaU']
+            event.beta_r = row['BetaR']
+            event.a_m = float(row['Am'])
+            event.building = row['Building']
+            event.floor = float(row['Floor'])
+            event.elevation = float(row['Elevation'])
+            event.area = row['Area']
+            event.seismic_fire = row['Seismic fire?']
+            event.seismic_flooding = row['Seismic flooding?']
+            event.count = row['count']
+            event.analysis_type = row['Analysis type']
+            event.phase_type = row['Phase Type']
+            event.project_name = row['Project']
+            event.start_time= float(row["Start"])
+            event.end_time = float(row["End"])
+
+            events.append(event)
+        return events
