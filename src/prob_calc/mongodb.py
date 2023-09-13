48956786136cc46fd2f46a072163226cb2caa1e2
Akram Batikh <asbatikh@ncsu.edu>
2023-09-13 13:59:24 -0400

    Database updated

--- src/prob_calc/mongodb.py
+++ src/prob_calc/mongodb.py
@@ -12,57 +12,103 @@ components_collection = db['components']
 flooding_sources_collection = db['flooding_sources']
 flooding_propagation_collection = db['flooding_propagation']
 
-# List of room names
-room_names = ["Room A", "Room B", "Room C", "Room D"]
+# Define buildings and floors
+buildings = ["Building A"]
+floors = [1, 2, 3]
+
+# List of common room names
+common_room_names = ["Room A", "Room B", "Room C", "Room D", "Room E", "Room F", "Room G", "Room H"]
 
 # Create random rooms
-# List of room names
-room_names = ["Room A", "Room B", "Room C", "Room D"]
+for building in buildings:
+    for floor_number in floors:
+        # Create unique room names for each floor based on the common room names
+        floor_room_names = [f"{common_name} - Floor {floor_number}" for common_name in common_room_names]
 
-# Create random rooms
-# Create random rooms
-for room_name in room_names:
-    room = {
-        "room_name": room_name,
-        "building_name": f"Building {random.randint(1, 5)}",
-        "floor_number": random.randint(1, 10)
-    }
-    room_id = rooms_collection.insert_one(room).inserted_id
+        for room_name in floor_room_names:
+            room = {
+                "room_name": room_name,
+                "building_name": building,
+                "floor_number": floor_number,
+                "flood_barrier": {
+                    "seismic_fragility": {
+                        "distribution_type": "L",  # Lognormal distribution
+                        "median_seismic_acceleration": random.uniform(0.1, 2.0),
+                        "beta_r_uncertainty": random.uniform(0.01, 0.5),
+                        "beta_u_uncertainty": random.uniform(0.01, 0.5)
+                    },
+                    "pressure_fragility": {
+                        "distribution_type": "F",  # Flooding distribution
+                        "median_flood_depth": random.uniform(1, 10),
+                        "beta_r_uncertainty": random.uniform(0.1, 2.0),
+                        "beta_u_uncertainty": random.uniform(0.1, 2.0)
+                    }
+                }
+            }
+            room_id = rooms_collection.insert_one(room).inserted_id
 
-    # Create random components for each room
-    for _ in range(random.randint(1, 5)):
-        component = {
-            "component_name": f"Component {random.randint(1, 10)}",
-            "room_id": room_id,
-            "vulnerability_details": "Component details here"
-        }
-        components_collection.insert_one(component)
+            # Create random components for each room
+            for _ in range(random.randint(1, 5)):
+                component = {
+                    "component_name": f"Component {random.randint(1, 10)}",
+                    "room_id": room_id,
+                    "vulnerability_details": {
+                        "fragility_model": [
+                            "L",  # Distribution type (Lognormal)
+                            random.uniform(0.1, 2.0),  # Median seismic acceleration (Am)
+                            random.uniform(0.01, 0.5),  # Beta_r uncertainty
+                            random.uniform(0.01, 0.5)   # Beta_u uncertainty
+                        ]
+                    }
+                }
+                components_collection.insert_one(component)
 
-    # Create random flooding sources for each room
-    for _ in range(random.randint(1, 3)):
-        source = {
-            "source_name": f"Flooding Source {random.randint(1, 5)}",
-            "room_id": room_id,
-            "source_details": "Source details here"
-        }
-        flooding_sources_collection.insert_one(source)
+            # Create random flooding sources for each room
+            for _ in range(random.randint(1, 3)):
+                source = {
+                    "source_name": f"Flooding Source {random.randint(1, 5)}",
+                    "room_id": room_id,
+                    "source_details": {
+                        "fragility_model": [
+                            "F",  # Distribution type (Flooding lognormal)
+                            random.uniform(1, 5),  # Median flood depth (D)
+                            random.uniform(0.1, 2.0),  # Beta_r uncertainty
+                            random.uniform(0.1, 2.0)   # Beta_u uncertainty
+                        ]
+                    }
+                }
+                flooding_sources_collection.insert_one(source)
 
-    # Create random flooding propagation events
-    for _ in range(random.randint(0, 2)):
-        source_rooms = random.sample(room_names, random.randint(1, len(room_names)))
-        target_room = random.choice(room_names)
-        while target_room in source_rooms:
-            target_room = random.choice(room_names)
-        source_room_ids = [rooms_collection.find_one({"room_name": source})["_id"] for source in source_rooms if rooms_collection.find_one({"room_name": source}) is not None]
-        target_room_id = rooms_collection.find_one({"room_name": target_room})["_id"] if rooms_collection.find_one({"room_name": target_room}) is not None else None
-        propagation = {
-            "source_rooms": source_rooms,
-            "target_room": target_room,
-            "source_room_ids": source_room_ids,
-            "target_room_id": target_room_id,
-            "propagation_details": "Propagation details here"
-        }
-        flooding_propagation_collection.insert_one(propagation)
+            # Create random flooding propagation events
+            for _ in range(random.randint(0, 2)):
+                source_rooms = random.sample(floor_room_names, random.randint(1, len(floor_room_names)))
+                target_room = random.choice(floor_room_names)
+                while target_room in source_rooms:
+                    target_room = random.choice(floor_room_names)
+
+                source_room_ids = []
+                for source in source_rooms:
+                    source_room_document = rooms_collection.find_one({"room_name": source})
+                    if source_room_document:
+                        source_room_ids.append(source_room_document["_id"])
+                    else:
+                        print(f"Warning: Room '{source}' not found in the database.")
+                        continue
+
+                target_room_document = rooms_collection.find_one({"room_name": target_room})
+                if target_room_document:
+                    target_room_id = target_room_document["_id"]
+                else:
+                    print(f"Warning: Room '{target_room}' not found in the database.")
+                    continue
+
+                propagation = {
+                    "source_rooms": source_rooms,
+                    "target_room": target_room,
+                    "source_room_ids": source_room_ids,
+                    "target_room_id": target_room_id
+                }
+                flooding_propagation_collection.insert_one(propagation)
 
 # Close the MongoDB connection
 client.close()
