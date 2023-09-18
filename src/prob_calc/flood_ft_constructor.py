afe49d7abbf806df88d200399403004bbd9662f3
Akram Batikh <asbatikh@ncsu.edu>
2023-09-18 14:18:42 -0400

    flood_ft_constructor for testing

---
+++ src/prob_calc/flood_ft_constructor.py
@@ -0,0 +1,71 @@
+from pymongo import MongoClient
+
+class SeismicFloodingFaultTree:
+    def __init__(self, mongodb_uri, db_name):
+        self.client = MongoClient(mongodb_uri)
+        self.db = self.client[db_name]
+        self.sources_collection = self.db["flood_sources"]
+        self.rooms_collection = self.db["rooms"]
+        self.barriers_collection = self.db["flood_barriers"]
+        self.flooding_gate_temp = self.db["flooding_gate_temp"]
+
+        # Check if the flood_HRA collection exists, and create it if it doesn't
+        if "Flooding_HRA" not in self.db.list_collection_names():
+            self.flood_HRA = None
+            print("The Flooding_HRA collection does not exist.")
+        else:
+            self.flood_HRA = self.db["Flooding_HRA"]
+
+    def flood_room_gate(self):
+        # Retrieve the template gate document (assuming it's the only one)
+        template_flood_room_gate = self.flooding_gate_temp.find_one({})
+
+        if not template_flood_room_gate:
+            print("Template flooding inside a room gate not found.")
+            return
+
+        # Use a cursor to iterate over every room document
+        cursor = self.rooms_collection.find({})
+
+        for room in cursor:
+            # Access the _id and name fields of each room document
+            room_id = room["_id"]
+            room_name = room["name"]
+
+            # Make a copy of the template gate to avoid modifying the original
+            modified_gate = template_flood_room_gate.copy()
+
+            # Replace placeholders with actual values
+            modified_gate["description"] = modified_gate["description"].replace("[room_num]", room_name)
+            modified_gate["name"] = modified_gate["name"].replace("[room_id]", str(room_id))
+
+            # Check if flood_HRA exists and find the document with room_id
+            if self.flood_HRA:
+                hra_document = self.flood_HRA.find_one({"room_id": room_id})
+                if hra_document:
+                    modified_gate.update_one(
+                        {"id": "SFR"},  # Filter to find the document with "id": "SFR"
+                        {"$push": {"inputs": hra_document}})
+
+            # Now, modified_gate contains the gate document with placeholders replaced
+            print(modified_gate)
+
+# Usage example
+def main():
+    mongodb_uri = 'mongodb+srv://akramsaid:Narcos99@myatlasclusteredu.nzilawl.mongodb.net/'
+    db_name = 'seismic_flooding_database'
+
+    tree = SeismicFloodingFaultTree(mongodb_uri, db_name)
+    tree.flood_room_gate()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
--- src/prob_calc/mongodb.py
+++ src/prob_calc/mongodb.py
@@ -9,6 +9,7 @@ class SeismicFloodingFaultTree:
         self.db = self.client[db_name]
         self.sources_collection = self.db["flood_sources"]
         self.rooms_collection = self.db["rooms"]
+        self.barriers_collection = self.db["flood_barriers"]
 
     def add_sources_of_flooding_gate(self, room_id, gate):
         room = self.rooms_collection.find_one({"_id": room_id})
@@ -29,7 +30,7 @@ class SeismicFloodingFaultTree:
 
         return gate
 
-    def generate_sources_gate_json(self, room_id):
+    def generate_sources_flooding_gate_json(self, room_id):
         gate = {
             "description": "",
             "name": "",
@@ -56,6 +57,62 @@ class SeismicFloodingFaultTree:
 
         return json_sources_gate
 
+    def add_barriers_gate(self, room_id, gate):
+        room = self.rooms_collection.find_one({"_id": room_id})
+        if room:
+            room_num = room.get("name", "")
+            gate_id = f"BARR-COLL-R-{room_id}-GT"
+            gate["description"] = f"BARRIERS OF ROOM {room_num}"
+            gate["name"] = gate_id
+            gate["id"] = "SFR"
+            gate["type"] = "GT"
+            gate["logic_type"] = "OR"
+            gate["inputs"] = []
+
+            barriers = self.barriers_collection.find({"room_id": room_id})
+            for barrier in barriers:
+                barrier_name = barrier.get("name", "")
+                barrier_id = barrier.get("_id", "")
+                seismic_fragility = barrier.get("failure_model", {}).get("seismic_fragility", {})
+                pressure_fragility = barrier.get("failure_model", {}).get("pressure_fragility", {})
+                barrier_gate = {
+                    "name": barrier_name,
+                    "id": barrier_id,
+                    "type": "FloodBarrierType",  # Replace with the actual barrier type
+                    "logic_type": "OR",  # Or whichever logic type is appropriate
+                    "seismic_fragility": seismic_fragility,
+                    "pressure_fragility": pressure_fragility,
+                }
+                gate["inputs"].append(barrier_gate)
+        return gate
+
+    def generate_barriers_gate_json(self, room_id):
+        gate = {
+            "description": "",
+            "name": "",
+            "id": "",
+            "type": "",
+            "logic_type": "",
+            "inputs": []
+        }
+
+        barriers_gate = self.add_barriers_gate(room_id, gate)
+
+        # Convert ObjectId to string representation before serializing to JSON
+        def convert_object_ids(obj):
+            if isinstance(obj, ObjectId):
+                return str(obj)
+            elif isinstance(obj, list):
+                return [convert_object_ids(item) for item in obj]
+            elif isinstance(obj, dict):
+                return {key: convert_object_ids(value) for key, value in obj.items()}
+            return obj
+
+        # Serialize the gate dictionary to JSON
+        json_barriers_gate = json.dumps(convert_object_ids(barriers_gate), indent=4)
+
+        return json_barriers_gate
+
 
 # Usage example
 mongodb_uri = 'mongodb+srv://akramsaid:Narcos99@myatlasclusteredu.nzilawl.mongodb.net/'
@@ -63,7 +120,9 @@ db_name = 'seismic_flooding_database'
 
 tree = SeismicFloodingFaultTree(mongodb_uri, db_name)
 room_id = ObjectId("65026bcc7358e5227605e791")  # Replace with the actual room_id
-json_sources_gate = tree.generate_sources_gate_json(room_id)
-print(json_sources_gate)
+json_sources_gate = tree.generate_sources_flooding_gate_json(room_id)
+json_barriers_gate = tree.generate_barriers_gate_json(room_id)
+
+print(json_barriers_gate)
 
 
